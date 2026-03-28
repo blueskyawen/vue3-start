45399296b1ad99385e2029c5373c45b2f476b278
凌晨 <16796380+zenthink@user.noreply.gitee.com>
2026-03-29 00:20:30 +0800

    add vue-codemorror

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,5 @@
-<script setup>
-import WelcomeItem from "./WelcomeItem.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
-import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
-
-const openReadmeInEditor = () => fetch("/__open-in-editor?file=README.md");
-</script>
-
 <template>
-  <WelcomeItem>
+<!--   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
     </template>
@@ -21,106 +10,84 @@ const openReadmeInEditor = () => fetch("/__open-in-editor?file=README.md");
       >official documentation</a
     >
     provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a
-      href="https://vite.dev/guide/features.html"
-      target="_blank"
-      rel="noopener"
-      >Vite</a
-    >. The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"
-      >VSCode</a
-    >
-    +
-    <a
-      href="https://github.com/vuejs/language-tools"
-      target="_blank"
-      rel="noopener"
-      >Vue - Official</a
-    >. If you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener"
-      >Playwright</a
-    >.
-
-    <br />
-
-    More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"
-      ><code>README.md</code></a
-    >.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener"
-      >Vue Router</a
-    >,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"
-      >Vue Test Utils</a
-    >, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"
-      >Vue Dev Tools</a
-    >. If you need more resources, we suggest paying
-    <a
-      href="https://github.com/vuejs/awesome-vue"
-      target="_blank"
-      rel="noopener"
-      >Awesome Vue</a
-    >
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
-    (our official Discord server), or
-    <a
-      href="https://stackoverflow.com/questions/tagged/vue.js"
-      target="_blank"
-      rel="noopener"
-      >StackOverflow</a
-    >. You should also follow the official
-    <a href="https://bsky.app/profile/vuejs.org" target="_blank" rel="noopener"
-      >@vuejs.org</a
-    >
-    Bluesky account or the
-    <a href="https://x.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    X account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its
-    sustainability. You can help us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"
-      >becoming a sponsor</a
-    >.
-  </WelcomeItem>
+  </WelcomeItem> -->
+  <div class="code-envc">
+    <div class="envc-header">运行</div>
+    <div class="envc-content">
+      <div class="code-editor">
+        <codemirror
+          v-model="code"
+          :style="{ height: '400px', width: '50%' }"
+          :extensions="extensions"
+          mode="text/javascript"
+          :disabled="disableCode"
+          @ready="handleReady"
+          @change="log('change', $event)"
+          @focus="log('focus', $event)"
+          @blur="log('blur', $event)"
+        />
+            </div>
+      <div class="code-result"></div>
+    </div>
+  </div>
 </template>
+<script>
+  import { defineComponent, ref, shallowRef } from 'vue'
+  import { Codemirror } from 'vue-codemirror'
+import { javascript } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { EditorView, lineNumbers } from "@codemirror/view";
+
+let myTheme = EditorView.theme({
+  "&": { color: "#0052D9", backgroundColor: "#FFFFFF" },
+  ".cm-content": { caretColor: "#0052D9" },
+  ".cm-activeLine": { backgroundColor: "#FAFAFA" },
+  ".cm-activeLineGutter": { backgroundColor: "#FAFAFA" },
+  "&.cm-focused .cm-cursor": { borderLeftColor: "#0052D9" },
+  "&.cm-focused .cm-selectionBackground, ::selection": {
+    backgroundColor: "#0052D9",
+    color: "#FFFFFF"
+  },
+  ".cm-gutters": { backgroundColor: "#FFFFFF", color: "#000", border: "none" }
+}, { dark: true });
+
+  export default defineComponent({
+    components: {
+      Codemirror
+    },
+    setup() {
+      const code = ref('')
+      const disableCode = ref(true)
+      const extensions = [javascript(), lineNumbers(), oneDark];
+
+      // Codemirror EditorView instance ref
+      const view = shallowRef()
+      const handleReady = (payload) => {
+        view.value = payload.view
+        console.log('编辑器已准备好', view.value)
+      }
+
+      // Status is available at all times via Codemirror EditorView
+      const getCodemirrorStates = () => {
+        const state = view.value.state
+        const ranges = state.selection.ranges
+        const selected = ranges.reduce((r, range) => r + range.to - range.from, 0)
+        const cursor = ranges[0].anchor
+        const length = state.doc.length
+        const lines = state.doc.lines
+        // more state info ...
+        // return ...
+      }
+
+      disableCode.value = false
+
+      return {
+        code,
+        disableCode,
+        extensions,
+        handleReady,
+        log: console.log
+      }
+    }
+  })
+</script>
--- src/main.js
+++ src/main.js
@@ -9,10 +9,19 @@ import router from "./router";
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 
+import VueCodemirror from 'vue-codemirror'
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(Antd);
+app.use(VueCodemirror, {
+  autofocus: true,
+  disabled: false,
+  indentWithTab: true,
+  tabSize: 2,
+  placeholder: '请输入代码...'
+});
 
 app.mount("#app");
